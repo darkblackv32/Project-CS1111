512c915470e5507218335ba6c23963361ab1850d
SantiagoGarfias <107209228+SantiagoGarfias@users.noreply.github.com>
2022-06-09 13:10:58 -0500

    Update empyworld.py

--- empyworld.py
+++ empyworld.py
@@ -1,13 +1,17 @@
+
 def draw_worldempty ():
     import colorama
     import colorama as init
-    from colorama import Back, Style, Fore
+    from colorama import Back, Style,Fore
+    import numpy as np
     colorama.init(autoreset=True)
-    world = [[j for  j in range(20)]for i in range(30)]
-    print(i for i in range(30))
-    for i in range(len(world)):
-        for j in range(len(world[i])):
-          print(Back.GREEN + ' ',end='')
+    world = [[Back.WHITE + '' for  j in range(31)]for i in range(21)]
+    world = np.array(world)
+
+    world[0] = [i for i in range(31)]
+
+    print('\n'.join([''.join(['{:4}'.format(columna) for columna in fila])
+                     for fila in world]))
 
 
 
