aabb10cefdfd6946d11e8d2eef9d8d94c9e68c0d
SantiagoGarfias <107209228+SantiagoGarfias@users.noreply.github.com>
2022-06-09 13:22:37 -0500

    Update empyworld.py

--- empyworld.py
+++ empyworld.py
@@ -1,4 +1,3 @@
-
 def draw_worldempty ():
     import colorama
     import colorama as init
@@ -7,8 +6,15 @@ def draw_worldempty ():
     colorama.init(autoreset=True)
     world = [[Back.WHITE + '' for  j in range(31)]for i in range(21)]
     world = np.array(world)
-
+    d = -1
     world[0] = [i for i in range(31)]
+    for i in range(len(world)):
+
+        d = d + 1
+        for j  in range (len(world[i])):
+
+            world[i][0] = d
+
 
     print('\n'.join([''.join(['{:4}'.format(columna) for columna in fila])
                      for fila in world]))
