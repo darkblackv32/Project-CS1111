5d95a4eb5f78b85a6418d02f847e8377f714b8b6
Soldi99 <103605443+Soldi99@users.noreply.github.com>
2022-06-10 20:56:49 -0500

    Vers_1.01

--- Día 002.py
+++ Día 002.py
@@ -2,22 +2,16 @@ from colorama import Back, Fore, Style, init
 
 init(autoreset=True)
 
-va = Back.LIGHTWHITE_EX + "    "
-wh = Back.LIGHTWHITE_EX + "    "
-bl = Back.BLACK + "    "
-gr = Back.GREEN + "    "
-ye = Back.YELLOW + "    "
-re = Back.RED + "    "
-bu = Back.BLUE + "    "
-lb = Back.LIGHTBLACK_EX + "    "
-cy = Back.CYAN + "    "
-ma = Back.MAGENTA + "    "
+
 def draw_world_empty():
-    mapa1 = [[]]
-    for i in range(1, 21):
-        print('')
-        for j in range(1, 31):
-            print(Back.WHITE + '   ', end = '')
+
+    for i in range(0, 22):
+        print('')                                           #     world = [[Back.LIGHTWHITE_EX + '' for j in range(0, 32)]for i in range(0, 22)]
+
+        for j in range(0, 32):
+            if j == 0:
+                print('')
+            print(Back.LIGHTWHITE_EX + '   ', end = '')
 
 
 draw_world_empty()
