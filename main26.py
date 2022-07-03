d95b4e3eae6a69c027964ba66bcd80c127475ed1
Soldi99 <103605443+Soldi99@users.noreply.github.com>
2022-07-03 13:34:34 -0700

    26.0.1
    Version 26.0.1

---
+++ main26.py
@@ -0,0 +1,19 @@
+from main260 import draw_worldempty, draw_world, move_player2, draw_world2empty, draw_world2
+
+
+def main():
+    draw_worldempty()
+    draw_world()  # llamamos a las funciones anteriores porque nos serviran pora que se ejecute
+    draw_world2empty()
+    draw_world2()
+    wood = 0
+    rock = 0
+
+    while True:
+        option = input('$ ').strip()  # se ingresa el init para iniciar el programa
+        if option == 'init':
+            break
+    move_player2(wood, rock)  # se ejecutan las funciones
+
+
+main()
